5b7c6657e98e44d19e8bde6728f23e7350620b26
ohara <ohara@3kaku.co.jp>
2018-07-30 18:00:04 +0900

    nodeテスト

---
+++ node/client.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+$(function() {
+    var server = require('http').createServer();
+    var io = require('socket.io')(server);
+
+	var socket = new io.Socket(null, {port: 8080});
+	socket.connect();
+
+	function iosHandleOrientation(event) {
+		var orientData = event.accelerationIncludingGravity;
+		socket.send(orientData.x + " " + orientData.y + " " + orientData.z );
+	}
+
+	window.addEventListener("devicemotion", iosHandleOrientation, true);
+
+	socket.on('message', function(obj) {
+		data = obj.split(" ");
+		$('#data').append("<tr><td>" + parseFloat(data[0]).toFixed(3) + "</td><td>" + parseFloat(data[1]).toFixed(3) + "</td><td>" + parseFloat(data[2]).toFixed(3) + "</td></tr>");
+
+	});
+});
